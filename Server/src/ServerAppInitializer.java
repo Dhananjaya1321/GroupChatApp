3e193d27ba36b1b9b8c0c28556e5d22aba5ab907
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-06-02 17:20:56 +0530

    serverController and server fxml file deleted

--- Server/src/ServerAppInitializer.java
+++ Server/src/ServerAppInitializer.java
@@ -1,19 +1,26 @@
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import lk.jise.liveChatApp.controller.ClientHandler;
 
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
-public class ServerAppInitializer extends Application {
-
+public class ServerAppInitializer {
+    private static Socket socket;
+    private static final ArrayList<ClientHandler> clients = new ArrayList<>();
     public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws IOException {
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("lk/jise/liveChatApp/view/Server.fxml"))));
-        primaryStage.show();
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+            while (true) {
+                System.out.println("Waiting for clients...");
+                socket = serverSocket.accept();
+                System.out.println("Connected");
+                ClientHandler clientThread = new ClientHandler(socket, clients);
+                clients.add(clientThread);
+                clientThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
